ac888a9f75cafd0ca82928622cad4395b208b966
Kenned Ferreira <74821126+kennedfer@users.noreply.github.com>
2024-12-30 12:24:50 +0000

    fix: Muda o Next para o modo de App Router

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import '../global.css'
+
+export default function Layout({ children }) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Next.js 15 App Router Layout</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
